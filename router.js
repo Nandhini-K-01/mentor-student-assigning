a82f4ba606a670ca90747bb291024af868bdcf93
Nandhini-K-01 <85298236+Nandhini-K-01@users.noreply.github.com>
2022-12-11 00:32:16 +0530

    Update router.js

--- router.js
+++ router.js
@@ -6,6 +6,9 @@ const router = express.Router();
 
 router.post("/creatementor", models.createMentor);
 router.post("/createstudent", models.createStudent);
+router.get("/getstudents", models.getStudent);
+router.get("/getmentors", models.getMentor);
+router.put("/assignstudent/:id", models.assignStudent);
 router.put("/changeorassignmentor/:id", models.changeorAssignMentor);
 router.get("/studentdetails", models.getAllStudents);
 
